e533e86fdded158ed28b063d108b594a822d1806
print-ramcharan <ramcharanpolabathina@gmail.com>
2026-01-07 16:47:40 +0530

    feat: integrate OpenAPI Swagger UI

--- src/main/java/com/keyurpandav/jobber/config/SecurityConfig.java
+++ src/main/java/com/keyurpandav/jobber/config/SecurityConfig.java
@@ -26,6 +26,9 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        // Swagger / OpenAPI
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        
                         .requestMatchers("/", "/home", "/register", "/css/**", "/js/**", "/images/**", "/login", "/jobs").permitAll()
                         .requestMatchers("/dashboard/user/**", "/applications/**").hasRole("APPLICANT")
                         .requestMatchers("/dashboard/employer/**", "/employer/**").hasRole("EMPLOYER")
